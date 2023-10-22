0c8e4d63299169de9d4b2da0753e6da08c5e635d
asus <asus@LAPTOP-5RR1SHUE>
2023-10-23 01:39:58 +0800

    Add project files.

---
+++ Piloton_Cashier/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Piloton_Cashier
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int moneyOwned;
+            int moneyPaid;
+            int changeAmount;
+
+            Console.Write("Enter the amount owned: ");
+            moneyOwned = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the amount paid: ");
+            moneyPaid = int.Parse(Console.ReadLine());
+
+            changeAmount = moneyPaid - moneyOwned;
+
+            if (moneyPaid < moneyOwned)
+            {
+                Console.WriteLine("Amount paid is less than the amount owned");
+            }
+            else
+            {
+                Console.WriteLine("Amount Change: " + changeAmount.ToString("F2"));
+            }
+        }
+    }
+}
